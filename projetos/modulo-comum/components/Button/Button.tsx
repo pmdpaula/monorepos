11ab1ca922e1525838193f1b308181aba703911a
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-04-21 14:07:51 -0300

    commit modulo06-aula01.1

---
+++ projetos/modulo-comum/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button>
+      Botão de @pmdpaula/modulo-comum
+    </button>
+  )
+}
+
+export default Button;
---
+++ projetos/site/pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+import moduleName from '@pmdpaula/modulo-comum/components/Button/Button';
+
+const Title = styled.h1`
+  color: blueviolet;
+  font-size: 50px;
+`
+
+export default function Home() {
+  return <Title>My page</Title>
+}
